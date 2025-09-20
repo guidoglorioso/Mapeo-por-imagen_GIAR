282f501b643e03a6d225aa58e5cee739f38e0f37
guidoglorioso <guido.glorioso@gmail.com>
2025-09-20 18:20:38 -0300

    Agrego estructura de proyecto

---
+++ scripts/libs/calib_sensor.py
@@ -0,0 +1,17 @@
+#######################################
+### Calibracion de imagen para eliminar distorsiones intrinsecas del lente (distorsión radial y tangencial).
+### Autor:
+### Fecha: 
+### Version: 1.0
+#######################################
+
+import cv2
+import numpy as np
+
+
+
+def CalibSensor():
+    pass
+
+if __name__ == "__main__":
+    pass
